2d0877559b4f04bd8085b03fe825d4daaa1650bf
escttsalo <escttsalo@gmail.com>
2021-06-14 03:29:27 -0600

    mvp

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import reducer from './reducers'
+
+const store = createStore(reducer, applyMiddleware(logger, thunk))
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store} >
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
